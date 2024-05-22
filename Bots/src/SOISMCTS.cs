dbadfa8783eedf1257588db0a38149539e39ad1c
owenant <anthonyowen1@gmail.com>
2024-05-22 15:53:10 +0100

    updated SOMISMCTS bot

--- Bots/src/SOISMCTS.cs
+++ Bots/src/SOISMCTS.cs
@@ -2,13 +2,29 @@ using ScriptsOfTribute;
 using ScriptsOfTribute.AI;
 using ScriptsOfTribute.Board;
 using ScriptsOfTribute.Serializers;
+using System.Diagnostics;
 
 namespace Bots;
 
 //class to implement single observer information set monte carlo tree search
 public class SOISMCTS : AI
 {
+    //note the seed should be removed so that it is based off the system clock, once the code is working
     private readonly SeededRandom rng = new(123);
+    private TimeSpan _usedTimeInTurn = TimeSpan.FromSeconds(0);
+    private TimeSpan _timeForMoveComputation = TimeSpan.FromSeconds(0.3);
+    private TimeSpan _TurnTimeout = TimeSpan.FromSeconds(10.0);
+    private PlayerEnum _myID;
+    private void PrepareForGame()
+    { 
+        //if any agent set-up needed it can be done here
+    }
+
+    public SOISMCTS()
+    {
+        this.PrepareForGame();
+    }
+    
     public override PatronId SelectPatron(List<PatronId> availablePatrons, int round)
     {
         return availablePatrons.PickRandom(rng);
@@ -16,11 +32,34 @@ public class SOISMCTS : AI
 
     public override Move Play(GameState gameState, List<Move> possibleMoves, TimeSpan remainingTime)
     {
-        //Initialise an information set based tree using current game state and the assumed observing player
-        FairSerializedPlayer observingPlayer = gameState.CurrentPlayer;
-        ISTree tree = new ISTree(gameState, observingPlayer); 
+        _myID = gameState.CurrentPlayer.PlayerID;
+        //Initialise a single node information set  tree using current game state as the root node
+        //and the assumed observing player
+        ISTree tree = new ISTree(gameState, _myID); 
+        
+        Stopwatch s = new Stopwatch();
+        s.Start();
+        while (s.Elapsed < _timeForMoveComputation)
+        {
+            //select a random game state from the information set root node of the tree with a uniform distribution
+            //we will use only nodes and actions compatible with this state
+            SeededGameState d0 = tree.GetRootDeterminsation((ulong) rng.Next());
+            
+            //enter selection routine - descend our IS tree (restricted to nodes/actions compatible with d0)
+            //using our chosen tree policy until a node is reached such that some action from that node leads to
+            //an observing player information set that is not currently in the tree or the node v is terminal
+            Tuple<ISNode, SeededGameState>  nodeGameState = select(tree, d0);
+            ISNode v = nodeGameState.Item1;
+            SeededGameState d = nodeGameState.Item2;
+        }
         
         return possibleMoves.PickRandom(rng);
+    }
+    
+    
+    public Tuple<ISNode, SeededGameState> select(ISTree v0, SeededGameState d0)
+    {
+        
     }
 
     public override void GameEnd(EndGameState state, FullGameState? finalBoardState)
@@ -37,14 +76,21 @@ public class ISTree
     //whose action it is in the current state that move our game from one state to the next
     public List<ISNode> _nodes;
     public List<ISLink> _links;
+    private ISNode _root;
     
     //constructor to initialise a tree with a root node using the current gameState as a reference state for the 
     //corresponding information set
-    public ISTree(GameState state, FairSerializedPlayer observingPlayer)
+    public ISTree(GameState state, PlayerEnum observingPlayer)
     {
         this._nodes = new List<ISNode>();
         ISNode rootNode = new ISNode(state, observingPlayer);
+        _root = rootNode;
         _nodes.Add(rootNode);
+    }
+
+    public SeededGameState GetRootDeterminsation(ulong seed)
+    {
+        return _root.Determinisation(seed);
     }
     
 }
@@ -55,15 +101,15 @@ public class ISNode
     private ISEquivalentStatesSet _infoSet;
     
     //create a node using a reference state for the information set it encapsulates
-    public ISNode(GameState refState, FairSerializedPlayer playerObserving)
+    public ISNode(GameState refState, PlayerEnum playerObserving)
     {
         this._infoSet = new ISEquivalentStatesSet(refState, playerObserving);
     }
     
     //return a random state from our node
-    public GameState Determinisation()
+    public SeededGameState Determinisation(ulong seed)
     {
-        return _infoSet.Determinsation();
+        return _infoSet.Determinisation(seed);
     }
 }
 
@@ -89,18 +135,21 @@ public class ISEquivalentStatesSet
 {
     //reference state, i.e. one instance of the set of equivalent states
     private GameState _refState;
-    private FairSerializedPlayer _observingPlayer;
+    private PlayerEnum _observingPlayer;
 
-    public ISEquivalentStatesSet(GameState state, FairSerializedPlayer playerObserving)
+    public ISEquivalentStatesSet(GameState state, PlayerEnum playerObserving)
     {
-        this._refState = state;
-        this._observingPlayer = playerObserving;
+        _refState = state;
+        _observingPlayer = playerObserving;
     }
 
     //returns a random state for our set of equivalent states
-    public GameState Determinsation()
+    public SeededGameState Determinisation(ulong seed)
     {
-        //TODO: How do we do this?
-        return this._refState;
+        //need to return a uniformly random pick from states in our equivalene set
+        
+        //TODO: does this actually return a random state from a uniform distribution across
+        //our information set?
+        return _refState.ToSeededGameState(seed);
     }
 }
