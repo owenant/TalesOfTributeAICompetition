7e61b1581edd4136d6d4f0a352e9aee66e8b65cc
owenant <anthonyowen1@gmail.com>
2024-05-28 09:16:29 +0100

    continuing to build initial code for SOISMCTS

--- Bots/src/SOISMCTS.cs
+++ Bots/src/SOISMCTS.cs
@@ -3,6 +3,7 @@ using ScriptsOfTribute.AI;
 using ScriptsOfTribute.Board;
 using ScriptsOfTribute.Serializers;
 using System.Diagnostics;
+using Newtonsoft.Json.Serialization;
 
 namespace Bots;
 
@@ -43,12 +44,13 @@ public class SOISMCTS : AI
         {
             //select a random game state from the information set root node of the tree with a uniform distribution
             //we will use only nodes and actions compatible with this state
-            SeededGameState d0 = tree.GetRootDeterminsation((ulong) rng.Next());
+            ISNode root = tree.GetRootNode();
+            SeededGameState d0 = root.GetDeterminisation((ulong) rng.Next());
             
             //enter selection routine - descend our IS tree (restricted to nodes/actions compatible with d0)
             //using our chosen tree policy until a node is reached such that some action from that node leads to
             //an observing player information set that is not currently in the tree or the node v is terminal
-            Tuple<ISNode, SeededGameState>  nodeGameState = select(tree, d0);
+            Tuple<ISNode, SeededGameState> nodeGameState = select(tree, d0);
             ISNode v = nodeGameState.Item1;
             SeededGameState d = nodeGameState.Item2;
         }
@@ -88,68 +90,101 @@ public class ISTree
         _nodes.Add(rootNode);
     }
 
-    public SeededGameState GetRootDeterminsation(ulong seed)
+    public ISNode GetRootNode()
     {
-        return _root.Determinisation(seed);
+        return _root;
     }
+    
+    //get all links from an IS node
+    
+    
+    //get all links from a node compatible with a determinisation d
+    
     
 }
 
 //Node for an ISTree, each node corresponds to an information set for the observing player for a tree
 public class ISNode
 {
-    private ISEquivalentStatesSet _infoSet;
+    //reference state, i.e. one instance of the set of equivalent states
+    private GameState _refState;
+    private PlayerEnum _observingPlayer;
     
     //create a node using a reference state for the information set it encapsulates
     public ISNode(GameState refState, PlayerEnum playerObserving)
     {
-        this._infoSet = new ISEquivalentStatesSet(refState, playerObserving);
+        this._refState = refState;
+        this._observingPlayer = playerObserving;
     }
     
-    //return a random state from our node
-    public SeededGameState Determinisation(ulong seed)
-    {
-        return _infoSet.Determinisation(seed);
-    }
-}
-
-//class to encapsulate links between ISNodes, these correspond to possible moves for the player about to play taking
-//the game state from a start node to an end node using a connectingMove
-public class ISLink
-{
-    public ISNode _startNode;
-    public ISNode _endNode;
-    public Move _connectingMove;
-
-    public ISLink(ISNode sourceNode, ISNode destinationNode, Move connectMove)
-    {
-        this._startNode = sourceNode;
-        this._endNode = destinationNode;
-        this._connectingMove = connectMove;
-    }
-}
-
-//this class can be constructed for a single reference state and then provide states that are information set
-//equivalent for the observing player 
-public class ISEquivalentStatesSet
-{
-    //reference state, i.e. one instance of the set of equivalent states
-    private GameState _refState;
-    private PlayerEnum _observingPlayer;
-
-    public ISEquivalentStatesSet(GameState state, PlayerEnum playerObserving)
-    {
-        _refState = state;
-        _observingPlayer = playerObserving;
-    }
-
-    //returns a random state for our set of equivalent states
-    public SeededGameState Determinisation(ulong seed)
+    //returns a random state (determinisation) for our set of equivalent states
+    public SeededGameState GetDeterminisation(ulong seed)
     {
         //need to return a uniformly random pick from states in our equivalene set
         
         //TODO: does this actually return a random state from a uniform distribution across
         //our information set?
         return _refState.ToSeededGameState(seed);
+    }
+    
+    //get children of a node (which are info sets) compatible with a determinatisation d
+    //equivalent of set c(v,d) in paper pseudo-code
+    public getCompatibleChildren(SeededGameState d)
+    {
+        
+    }
+}
+
+//class to encapsulate links between ISNodes, these correspond to an action that can can be taken for the
+//player to move from a starting information set to an end info set
+public class ISLink
+{
+    public ISNode _startISNode;
+    public ISNode _endISNode;
+    public ISAction _connectingISAction;
+
+    public ISLink(ISNode sourceNode, ISNode destinationNode, ISAction connectISAction)
+    {
+        this._startISNode = sourceNode;
+        this._endISNode = destinationNode;
+        this._connectingISAction = connectISAction;
+    }
+}
+
+//an action is an equivalence class of edges. Each edge is a link between two specific game states 
+//(determinisations). Two edges are equivalent if 1. They do not belong to the same starting determinisation
+//2. The start info set is the same for both edges 3. The end info set is the same for both edges
+//4/ All edges in an action have the same player about to start (corresponding to player for start ISNode)
+//TODO: I dont understand point 1?
+public class ISAction
+{
+    private ISNode _startISNode;
+    private ISNode _endISNode;
+    private Edge _refEdge;
+    
+    public ISAction(ISNode startNode, ISNode endNode, Edge refEdge)
+    {
+        this._startISNode = startNode;
+        this._endISNode = endNode;
+        this._refEdge = refEdge;
+        
+        //TODO: check start and end state of refEdge are part of the startNode and endNode information sets
+    }
+    
+    //check this action is compatible with a particular determinisation of the startNode
+
+}
+
+//this defines a specific move between two concrete game states or determinisations
+public class Edge
+{
+    private SeededGameState _startState;
+    private SeededGameState _endState;
+    private Move _move;
+    public Edge(SeededGameState startState, SeededGameState endState, Move playerMove)
+    {
+        this._startState = startState;
+        this._endState = endState;
+        this._move = playerMove;
     }
 }
