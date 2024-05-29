2dafcf3ef45df79fd0d18fc2b1906910ec3dd542
owenant <anthonyowen1@gmail.com>
2024-05-29 12:05:00 +0100

    continuing SOISMCTS build out

--- Bots/src/SOISMCTS.cs
+++ Bots/src/SOISMCTS.cs
@@ -3,7 +3,7 @@ using ScriptsOfTribute.AI;
 using ScriptsOfTribute.Board;
 using ScriptsOfTribute.Serializers;
 using System.Diagnostics;
-using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
 
 namespace Bots;
 
@@ -19,6 +19,9 @@ public class SOISMCTS : AI
     private void PrepareForGame()
     { 
         //if any agent set-up needed it can be done here
+        
+        //TODO: can we initialise and store a static tree object so that we can re-use the tree from 
+        //move to move?
     }
 
     public SOISMCTS()
@@ -38,30 +41,66 @@ public class SOISMCTS : AI
         //and the assumed observing player
         ISTree tree = new ISTree(gameState, _myID); 
         
+        //select a random game state from the information set root node of the tree with a uniform distribution
+        //we will use only nodes and actions compatible with this state
+        ISNode v = tree.GetRootNode();
+        SeededGameState d = v.GetDeterminisation((ulong) rng.Next());
+        //TODO:: get moves compatible with this determinisation
+        List<Move> compatibleMoves = possibleMoves;
+        
         Stopwatch s = new Stopwatch();
         s.Start();
         while (s.Elapsed < _timeForMoveComputation)
         {
-            //select a random game state from the information set root node of the tree with a uniform distribution
-            //we will use only nodes and actions compatible with this state
-            ISNode root = tree.GetRootNode();
-            SeededGameState d0 = root.GetDeterminisation((ulong) rng.Next());
-            
-            //enter selection routine - descend our IS tree (restricted to nodes/actions compatible with d0)
-            //using our chosen tree policy until a node is reached such that some action from that node leads to
-            //an observing player information set that is not currently in the tree or the node v is terminal
-            Tuple<ISNode, SeededGameState> nodeGameState = select(tree, d0);
-            ISNode v = nodeGameState.Item1;
-            SeededGameState d = nodeGameState.Item2;
+            //enter selection routine - return a node along with determinisation and compatible move set
+            Tuple<ISNode, SeededGameState, List<Move>> nodeGameStateAndMoves = select(tree, v, d, compatibleMoves);
+            v = nodeGameStateAndMoves.Item1;
+            d = nodeGameStateAndMoves.Item2;
+            compatibleMoves = nodeGameStateAndMoves.Item3;
         }
         
         return possibleMoves.PickRandom(rng);
     }
     
     
-    public Tuple<ISNode, SeededGameState> select(ISTree v0, SeededGameState d0)
-    {
+    public Tuple<ISNode, SeededGameState> select(ISTree tree, ISNode v, SeededGameState d, List<Move> compatibleMoves)
+    { 
+        //descend our IS tree (restricted to nodes/actions compatible with d)using our chosen tree policy
+        //until a node is reached such that some action from that node leads to
+        //an observing player information set that is not currently in the tree or the node v is terminal
         
+        double val = 0;
+        double bestVal = 0;
+        ISNode bestNode = v;
+        HashSet<Action> uvd = tree.GetActionsWithNoTreeNodes(v, d);
+        while (d.GameEndState != null && uvd.Count == 0)
+        {
+            HashSet<ISNode> cvd = tree.GetCompatibleChildrenInTree(v, d, compatibleMoves);
+            foreach(ISNode node in cvd)
+            {
+                val = TreePolicy(node);
+                if (val > bestVal)
+                {
+                    bestVal = val;
+                    bestNode = node;
+                }
+            }
+            v = bestNode;
+            //TODO: update determinisation
+            ISAction incomingAction = tree.GetIncomingActionForNodeInTree(v);
+            //TODO: shift from our Action to List<Moves> used by engine
+            //var (newSeedGameState, newPossibleMoves) = seedGameState.ApplyMove(nextMove);
+            
+            d = d;
+            uvd = tree.GetActionsWithNoTreeNodes(v, d);
+        }
+        Tuple<ISNode, SeededGameState> selectedNodeAndState = new Tuple<ISNode, SeededGameState>(v, d);
+        return selectedNodeAndState;
+    }
+
+    private double TreePolicy(ISNode node)
+    {
+        return 0;
     }
 
     public override void GameEnd(EndGameState state, FullGameState? finalBoardState)
@@ -95,12 +134,48 @@ public class ISTree
         return _root;
     }
     
-    //get all links from an IS node
+    //get children of a node (which are info sets) compatible with a determinatisation d
+    //(equivalent of c(v,d) in paper pseudo-code)
+    public HashSet<ISNode> GetCompatibleChildrenInTree(ISNode v, SeededGameState d, List<Move> compatibleMoves)
+    {
+        HashSet<ISNode> compatibleChildren = new HashSet<ISNode>();
+        //var (newSeedGameState, newPossibleMoves) = seedGameState.ApplyMove(nextMove);
+        foreach (ISLink link in _links)
+        {
+            if (link._startISNode.Equals(v))
+            {
+                
+                return link._connectingISAction;
+            }
+        }
+
+        return compatibleChildren;
+    }
     
-    
-    //get all links from a node compatible with a determinisation d
-    
-    
+    //get actions from a node v and determinisation d, for which v does not have children in the tree
+    //equivalent to function u(v,d) in pseudo-code
+    public HashSet<Action> GetActionsWithNoTreeNodes(ISNode v, SeededGameState d)
+    {
+        HashSet<Action> compatibleActions = v.GetCompatibleActions(d);
+        
+        //TODO:find actions that have no node in the tree
+        HashSet<Action> actionWithNoTreeNode = new HashSet<Action>();
+        
+        return actionWithNoTreeNode;
+    }
+
+    public ISAction GetIncomingActionForNodeInTree(ISNode v)
+    {
+        //check node is in tree
+        foreach (ISLink link in _links)
+        {
+             if (link._endISNode.Equals(v))
+             {
+                 return link._connectingISAction;
+             }
+         }
+         throw new Exception("GetIncomingActionForNodeInTree: Node not found in ISTree");
+    }
 }
 
 //Node for an ISTree, each node corresponds to an information set for the observing player for a tree
@@ -126,12 +201,36 @@ public class ISNode
         //our information set?
         return _refState.ToSeededGameState(seed);
     }
-    
-    //get children of a node (which are info sets) compatible with a determinatisation d
-    //equivalent of set c(v,d) in paper pseudo-code
-    public getCompatibleChildren(SeededGameState d)
+
+    //get actions available from info set, compatible with determinisation d. This is A(d) 
+    //in pseudo-code. Strictly speaking the state d does not need to be a determinisation of
+    //an info set, hence A is only a function of d. But coding wise it seems appropriate to place
+    //the method here
+    public HashSet<Action> GetCompatibleActions(SeededGameState d)
     {
-        
+        HashSet<Action> compatibleActions = new HashSet<Action>();
+        return compatibleActions;
+    }
+
+    // Override Equals method to define equality
+    public override bool Equals(object obj)
+    {
+        ISNode node = (ISNode)obj;
+        //check if reference states are equivalent
+        if (this._refState.Equals(node._refState) && node._observingPlayer == this._observingPlayer)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Override GetHashCode method to ensure consistency with Equals
+    public override int GetHashCode()
+    {
+        return (this._refState, this._observingPlayer).GetHashCode();
     }
 }
 
@@ -154,7 +253,7 @@ public class ISLink
 //an action is an equivalence class of edges. Each edge is a link between two specific game states 
 //(determinisations). Two edges are equivalent if 1. They do not belong to the same starting determinisation
 //2. The start info set is the same for both edges 3. The end info set is the same for both edges
-//4/ All edges in an action have the same player about to start (corresponding to player for start ISNode)
+//4 All edges in an action class have the same player about to start (corresponding to player for start ISNode)
 //TODO: I dont understand point 1?
 public class ISAction
 {
